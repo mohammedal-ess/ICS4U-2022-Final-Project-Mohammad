f9a23200a089a221b5886fe6bb5e9e0fbc6e1efa
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2023-01-10 14:04:21 +0000

    adding a splashscene change command in game

--- js/game.js
+++ js/game.js
@@ -6,24 +6,37 @@
 // Created on: Dec 2022
 // This is the phaser3 configuration file
 
+import splashScene from './splashScene.js'
+
+// Our game scene 
+const splashScene = new SplashScene()
+//* Game scene */
 const config = {
-    type: Phaser.Auto,
-    width: 1920,
-    height: 1080,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        debug: true
-      }
-    },
-    //set backgroundColor
-    backgroundColor: 0x5f6e7a,
-    scale:{
-      mode: Phaser.Scale.FIT,
-      // we place it in the middle of the page
-      autoCenter: Phaser.Scale.CENTER_BOTH
+  type: Phaser.Auto,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
     }
+  },
+  //set backgroundColor
+  backgroundColor: 0x5f6e7a,
+  scale:{
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
   }
-  
-  const game = new Phaser.Game(config)
-  console.log(game)
+}
+
+const game = new Phaser.Game(config)
+
+
+// load scenes 
+// NOTE: remember any "key" is global and CAN NOT be reused!
+game.scene.add('splashScene', splashScene)
+
+// start title
+game.scene.start('splashScene')
+
