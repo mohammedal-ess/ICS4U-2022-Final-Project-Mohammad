6724d29be4638e052d5b6b5a293c6e079394bfa4
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2023-01-12 14:03:20 +0000

    ADD TEXT IN TITLE

--- js/titleScene.js
+++ js/titleScene.js
@@ -11,6 +11,8 @@ class Titlescene extends Phaser.Scene {
     super({key: 'titleScene'})
 
   this.titleSceneBackgroundImage = null
+  this.titleSceneText = null 
+  this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center' }
   }
   
 init (data) {
@@ -26,6 +28,9 @@ create (data) {
     this.titleSceneBackgroundImage = this.add.sprite(0, 0, 'titleSceneBackground')
     this.titleSceneBackgroundImage.x = 1920 / 2
     this.titleSceneBackgroundImage.y = 1080 / 2
+
+  this/titleSceneText = this.add.text(1920 / 2, (1080/ 2) + 350, 'Space Aliens', 
+  this.titleSceneTextStyle).setOrigin(0.5)
 }
 
  update (time, delta) {  
